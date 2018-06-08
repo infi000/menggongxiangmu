56c2c14492505380e15ecf2b3b51c79bbdcd302b
infiooo <113079767@qq.com>
2018-06-08 19:06:55 +0800

    work

--- static/js/main.js
+++ static/js/main.js
@@ -29,9 +29,9 @@ function createDOM_navModal(argument) {
     var html = '<div class="g-hd-modal-con" datatype="about" style="display: none">\
                     <div class="g-hd-modal-l">\
                         <ul>\
-                            <li class="f-bg-main"><a href="">公司简介 <embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
-                            <li><a href="">企业文化<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
-                            <li><a href="">企业资质<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
+                            <li class="f-bg-main"><a href="./about.html">公司简介 <embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
+                            <li><a href="./about.html">企业文化<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
+                            <li><a href="./about.html">企业资质<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
                         </ul>\
                     </div>\
                     <div class="g-hd-modal-r">\
@@ -104,7 +104,7 @@ function createDOM_navModal(argument) {
                     <div class="g-hd-modal-l">\
                         <ul>\
                             <li class="f-bg-main"><a href="">企业新闻 <embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
-                            <li><a href="">业内新闻<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
+                            <li style="display:none"><a href="">业内新闻<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
                         </ul>\
                     </div>\
                     <div class="g-hd-modal-r">\
@@ -114,11 +114,11 @@ function createDOM_navModal(argument) {
                                 <dd> </dd>\
                             </dl>\
                             <ul class="title-link">\
-                                <li><a href="">镝普为东莞产业转型升级树立典范</a><span>2017-11-23</span></li>\
-                                <li><a href="">洞察企业转型之道 助力企业转型之举的主线</a><span>2017-08-22</span></li>\
-                                <li><a href="">中企跨境并购十年回顾</a><span>2017-08-20</span></li>\
-                                <li><a href="">潘家铮获第九届光华工程科技奖成就奖</a><span>2017-09-29</span></li>\
-                                <li><a href="">潘家铮获第九届光华工程科技奖成就奖</a><span>2017-09-22</span></li>\
+                                <li><a href="./newsdetail.html?num=0">人工智能带给各行业的冲击和机遇</a><span>2018-6-4</span></li>\
+                                <li><a href="./newsdetail.html?num=1">浅谈软件定义卫星</a><span>2018-5-13</span></li>\
+                                <li><a href="./newsdetail.html?num=2">你附近有什么标识？”——基于邻近相关性的搜寻</a><span>2018-4-10</span></li>\
+                                <li><a href="./newsdetail.html?num=3">浅谈军事航天气象保障</a><span>2018-3-10</span></li>\
+                                <li><a href="./newsdetail.html?num=4">“量子”计算改变我们的生活</a><span>2018-2-2</span></li>\
                             </ul>\
                         </div>\
                     </div>\
@@ -128,7 +128,7 @@ function createDOM_navModal(argument) {
                     <div class="g-hd-modal-l">\
                         <ul>\
                             <li class="f-bg-main"><a href="">服务支持 <embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
-                            <li><a href="">加入我们<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
+                            <li><a href="./contactjob.html">加入我们<embed src="./static/img/arrow.svg" type="image/svg+xml" pluginspage="http://www.adobe.com/svg/viewer/install/" /></a></li>\
                         </ul>\
                     </div>\
                     <div class="g-hd-modal-r">\
@@ -138,7 +138,7 @@ function createDOM_navModal(argument) {
                                 <dd></dd>\
                             </dl>\
                             <ul class="title-link">\
-                                <li>联系电话：+68 13810586885</li>\
+                                <li>联系电话：010-57252161</li>\
                                 <li>联系邮箱： service@deeputech.com</li>\
                                 <li>地理位置: 北京市 西城区 红居街13号 楼翔峰大厦5层</li>\
                             </ul>\
